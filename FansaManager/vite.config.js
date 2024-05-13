35b04daf3ff6ebb4aba90bb32c472a002bc011eb
phuccoderr <phuctapcode@gmail.com>
2024-05-13 19:06:49 +0700

    up dockerfile

--- FansaManager/vite.config.js
+++ FansaManager/vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  // server: {
+  //   host: true,
+  //   strictPort: true,
+  //   port: 3000,
+  // },
+  preview: {
+    host: true,
     port: 3000,
-  }
-})
+  },
+});
